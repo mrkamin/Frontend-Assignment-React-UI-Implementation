e5561f5160af4bca3a5b0ca4e1ad8037adcfb984
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-09 16:00:40 +0500

    Define Actions type

---
+++ src/redux/actionTypes/dataActionTypes.js
@@ -0,0 +1,7 @@
+const ActionTypes = {
+  FETCH_DATA_REQUEST: "FETHC_DATA_REQUEST",
+  FETCH_DATA_SUCCESS: "FETCH_DATA_SUCCESS",
+  FETCH_DATA_FAILURE: "FETCH_DATA_FAILURE",
+};
+
+export default ActionTypes;
