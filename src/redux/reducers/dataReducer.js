86df08a53a9786d180ade89307335008a3021d57
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-09 16:01:35 +0500

    Define Reducer

---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,21 @@
+import ActionTypes from "../actionTypes/dataActionTypes";
+
+const initialState = {
+  data: [
+    {
+      id: 1,
+      title: "Rafi",
+      category: "programmer",
+    },
+  ],
+};
+
+const dataReducer = (state, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.FETCH_DATA_REQUEST:
+      return state;
+    default:
+      return state;
+  }
+};
+export default dataReducer;
