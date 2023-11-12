4ee018c66e858c7ae44e41b139846951c97440a8
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-12 20:15:53 +0500

    Creat Footer Compnent

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -5,6 +6,7 @@ const App = () => (
   <div className="App">
     <Navbar />
     <Hero />
+    <Footer />
   </div>
 );
 
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import ConversionRate from './ConversionRate';
+import Location from './Location';
+import WebsiteAnalytics from './WebsiteAnalytics';
+
+const Footer = () => (
+  <div className=" footer grid1">
+    <Location />
+    <ConversionRate />
+    <WebsiteAnalytics />
+  </div>
+);
+
+export default Footer;
