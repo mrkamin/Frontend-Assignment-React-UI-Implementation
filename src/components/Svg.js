742e050cedbff9491fa5267a76935a4c3de20a46
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-12 12:44:10 +0500

    Make the chart

--- src/components/Svg.js
+++ src/components/Svg.js
@@ -1,34 +1,263 @@
 const Svg = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="svg" viewBox="0 0 100 30">
-    {/* Y-axis (Percentage) */}
-    <text x="5%" y="5%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">200%</text>
-    <text x="5%" y="17%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">100%</text>
-    <text x="5%" y="29%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">50%</text>
-    <text x="5%" y="41%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">0%</text>
-    <text x="5%" y="53%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">-50%</text>
-    <text x="5%" y="65%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">-100%</text>
-    <text x="5%" y="82%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">-200%</text>
+  <svg xmlns="http://www.w3.org/2000/svg" className="svg" viewBox="0 0 100 27">
+    <circle cx="10.3" cy="1.75" r="0.4" fill="#00F2DE" />
+    <circle cx="18.73" cy="14.7" r="0.4" fill="#00F2DE" />
+    <circle cx="26.6" cy="9.8" r="0.4" fill="#00F2DE" />
+    <circle cx="34.4" cy="16" r="0.4" fill="#00F2DE" />
+    <circle cx="42.5" cy="21.5" r="0.4" fill="#00F2DE" />
+    <circle cx="50.5" cy="7.5" r="0.4" fill="#00F2DE" />
+    <circle cx="58.5" cy="4.5" r="0.4" fill="#00F2DE" />
+    <circle cx="66.5" cy="8.3" r="0.4" fill="#00F2DE" />
+    <circle cx="74.5" cy="13.8" r="0.4" fill="#00F2DE" />
+    <circle cx="82.5" cy="15.5" r="0.4" fill="#00F2DE" />
+    <circle cx="90.5" cy="8.3" r="0.4" fill="#00F2DE" />
+    <circle cx="98.5" cy="5.8" r="0.4" fill="#00F2DE" />
+    <line
+      x1="10.6%"
+      y1="8%"
+      x2="18.6%"
+      y2="55%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="18.6%"
+      y1="55%"
+      x2="26.6%"
+      y2="36%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="26.6%"
+      y1="36%"
+      x2="34.6%"
+      y2="60%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="34.6%"
+      y1="60%"
+      x2="42.6%"
+      y2="80%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="50.6%"
+      y1="27.5%"
+      x2="42.6%"
+      y2="80%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="50.6%"
+      y1="27.5%"
+      x2="58.6%"
+      y2="17%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="66.6%"
+      y1="30.5%"
+      x2="58.6%"
+      y2="17%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="66.6%"
+      y1="30.5%"
+      x2="74.6%"
+      y2="51%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="82.6%"
+      y1="58%"
+      x2="74.6%"
+      y2="51%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="82.6%"
+      y1="58%"
+      x2="90.6%"
+      y2="30%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <line
+      x1="98.6%"
+      y1="21%"
+      x2="90.6%"
+      y2="30%"
+      stroke="#00F2DE"
+      strokeDasharray="0.5"
+      strokeWidth="0.08"
+      opacity="1"
+    />
+    <text x="1%" y="5%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      200%
+    </text>
+    <text x="1%" y="19%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      100%
+    </text>
+    <text x="1%" y="33%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      50%
+    </text>
+    <text x="1%" y="46%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      0%
+    </text>
+    <text x="1%" y="60%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      -50%
+    </text>
+    <text x="1%" y="74%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      -100%
+    </text>
+    <text x="1%" y="88%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      -200%
+    </text>
 
-    {/* X-axis (Months) */}
-    <text x="10%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Jan</text>
-    <text x="18%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Feb</text>
-    <text x="26%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Mar</text>
-    <text x="34%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Apr</text>
-    <text x="42%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">May</text>
-    <text x="50%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Jun</text>
-    <text x="58%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Jul</text>
-    <text x="66%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Aug</text>
-    <text x="74%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Sep</text>
-    <text x="82%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Oct</text>
-    <text x="90%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Nov</text>
-    <text x="98%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">Dec</text>
+    <text x="10%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Jan
+    </text>
+    <text x="18%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Feb
+    </text>
+    <text x="26%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Mar
+    </text>
+    <text x="34%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Apr
+    </text>
+    <text x="42%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      May
+    </text>
+    <text x="50%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Jun
+    </text>
+    <text x="58%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Jul
+    </text>
+    <text x="66%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Aug
+    </text>
+    <text x="74%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Sep
+    </text>
+    <text x="82%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Oct
+    </text>
+    <text x="90%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Nov
+    </text>
+    <text x="98%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Dec
+    </text>
 
-    {/* Graph bars */}
-    {/* Example bar for January, representing 50% */}
-    <rect x="10%" y="35%" width="20%" height="3%" fill="green" />
+    <rect x="9%" y="10%" width="3%" height="36%" fill="#0082CC" />
+    <rect x="25%" y="41%" width="3%" height="5%" fill="#0082CC" />
+    <rect x="49%" y="30%" width="3%" height="16%" fill="#0082CC" />
+    <rect x="57%" y="20%" width="3%" height="26%" fill="#0082CC" />
+    <rect x="65%" y="33%" width="3%" height="13%" fill="#0082CC" />
+    <rect x="89%" y="33%" width="3%" height="13%" fill="#0082CC" />
+    <rect x="97%" y="25%" width="3%" height="21%" fill="#0082CC" />
 
-    {/* Example bar for February, representing -20% */}
-    <rect x="40%" y="65%" width="10%" height="3%" fill="red" />
+    <rect x="17%" y="46%" width="3%" height="5%" fill="#FB4540" />
+    <rect x="33%" y="46%" width="3%" height="10%" fill="#FB4540" />
+    <rect x="41%" y="46%" width="3%" height="30%" fill="#FB4540" />
+    <rect x="73%" y="46%" width="3%" height="3%" fill="#FB4540" />
+    <rect x="81%" y="46%" width="3%" height="8%" fill="#FB4540" />
+
+    <line
+      x1="5%"
+      y1="5%"
+      x2="100%"
+      y2="5%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="5%"
+      y1="19%"
+      x2="100%"
+      y2="19%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="5%"
+      y1="33%"
+      x2="100%"
+      y2="33%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="5%"
+      y1="46%"
+      x2="100%"
+      y2="46%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="5%"
+      y1="60%"
+      x2="100%"
+      y2="60%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="5%"
+      y1="74%"
+      x2="100%"
+      y2="74%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="5%"
+      y1="88%"
+      x2="100%"
+      y2="88%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
   </svg>
 );
 
