767a4c0e61fb2e17b4bab1dc548de4249460f374
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-12 21:46:51 +0500

    Create Location

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,5 +1,41 @@
+import React from 'react';
+import SvgLocation from './SvgLocation';
+
 const Location = () => (
-  <h1>Location</h1>
+  <div className="hero-container-right-side d-flex flex-column">
+    <div className="right-side-uper-head d-flex">
+      <div className="uper-head-right-side">
+        <h4>Location</h4>
+      </div>
+      <div className="uper-head-left-side d-flex">
+        <div className="left-side-first-item d-flex">
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            className=""
+          >
+            <circle cx="10" cy="10" r="5" fill="#00F2DE" />
+          </svg>
+          <div className="white-dark">Country</div>
+        </div>
+        <div className="left-side-first-item d-flex">
+          <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            className=""
+          >
+            <circle cx="10" cy="10" r="5" fill="#FB4540" />
+          </svg>
+          <div className="white-dark">City</div>
+        </div>
+      </div>
+    </div>
+    <div className="right-side-body">
+      <SvgLocation />
+    </div>
+  </div>
 );
 
 export default Location;
---
+++ src/components/SvgLocation.js
@@ -0,0 +1,130 @@
+const SvgLocation = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="svg"
+    viewBox="0 0 100 21"
+  >
+    <line
+      x1="5%"
+      y1="5%"
+      x2="5%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="11%"
+      y1="5%"
+      x2="11%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="17%"
+      y1="5%"
+      x2="17%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="23%"
+      y1="5%"
+      x2="23%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="29%"
+      y1="5%"
+      x2="29%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="35%"
+      y1="5%"
+      x2="35%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="41%"
+      y1="5%"
+      x2="41%"
+      y2="80%"
+      stroke="#AEBBDA"
+      strokeDasharray="0.3"
+      strokeWidth="0.02"
+    />
+    <line
+      x1="4%"
+      y1="88%"
+      x2="43%"
+      y2="88%"
+      stroke="#AEBBDA"
+      strokeWidth="0.02"
+    />
+    <rect x="4.5%" y="46%" width="1%" height="42%" fill="#00F2DE" />
+    <rect x="4.5%" y="65%" width="1%" height="23%" fill="#FB4540" />
+    <rect x="10.5%" y="35%" width="1%" height="52.7%" fill="#00F2DE" />
+    <rect x="10.5%" y="60%" width="1%" height="28%" fill="#FB4540" />
+    <rect x="16.5%" y="25%" width="1%" height="62.7%" fill="#00F2DE" />
+    <rect x="16.5%" y="55%" width="1%" height="33%" fill="#FB4540" />
+    <rect x="22.5%" y="15%" width="1%" height="73%" fill="#00F2DE" />
+    <rect x="22.5%" y="50%" width="1%" height="38%" fill="#FB4540" />
+    <rect x="28.5%" y="15%" width="1%" height="73%" fill="#00F2DE" />
+    <rect x="28.5%" y="45%" width="1%" height="43%" fill="#FB4540" />
+    <rect x="34.5%" y="25%" width="1%" height="62.7%" fill="#00F2DE" />
+    <rect x="34.5%" y="50%" width="1%" height="38%" fill="#FB4540" />
+    <rect x="40.5%" y="46%" width="1%" height="42%" fill="#00F2DE" />
+    <rect x="40.5%" y="65%" width="1%" height="23%" fill="#FB4540" />
+
+    <text x="1%" y="5%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      $3k
+    </text>
+    <text x="1%" y="30%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      $2k
+    </text>
+    <text x="1%" y="55%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      $1k
+    </text>
+    <text x="1%" y="80%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      0
+    </text>
+
+    <text x="4.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Mo
+    </text>
+    <text x="10.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Tu
+    </text>
+    <text x="16.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      We
+    </text>
+    <text x="22.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Th
+    </text>
+    <text x="28.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Fr
+    </text>
+    <text x="34.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Sa
+    </text>
+    <text x="40.5%" y="95%" fontFamily="Arial" fontSize="5%" fill="#AEBBDA">
+      Su
+    </text>
+  </svg>
+);
+
+export default SvgLocation;
