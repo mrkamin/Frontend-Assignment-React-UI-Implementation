55b514e70d288cc6e305055345633f4b8b5f2e77
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-13 09:39:45 +0500

    Map Data

--- src/components/ConversionRate.js
+++ src/components/ConversionRate.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchData } from '../redux/data/actions';
 import SvgConverrsionRate from './SvgConversionRate';
 
-const ConversionRate = () => (
-  <div className="hero-container-right-side d-flex flex-column">
-    <div className="right-side-uper-head d-flex">
-      <div className="uper-head-right-side">
-        <h4>Conversion Rate</h4>
-      </div>
-    </div>
-    <div className="right-side-body grid2">
-      <SvgConverrsionRate />
+const ConversionRate = () => {
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector((state) => state);
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return (
       <div>
-        <div>
-          <h5>Total User Visit</h5>
-          <p>234324234</p>
+        Error:
+        {error}
+      </div>
+    );
+  }
+  console.log('show data', data.conversionRate.totalUserVisit);
+  return (
+    <div className="hero-container-right-side d-flex flex-column">
+      <div className="right-side-uper-head d-flex">
+        <div className="uper-head-right-side">
+          <h4>Conversion Rate</h4>
         </div>
+      </div>
+      <div className="right-side-body grid2">
+        <SvgConverrsionRate />
         <div>
-          <h5>Total User Visit</h5>
-          <p>234324234</p>
-        </div>
-        <div>
-          <h5>Total User Visit</h5>
-          <p>234324234</p>
+          <div>
+            <h5>Total User Visit</h5>
+            <p>{data.conversionRate.userSignUp}</p>
+          </div>
+          <div>
+            <h5>Total User Visit</h5>
+            <p>{data.conversionRate.totalUserVisit}</p>
+          </div>
+          <div>
+            <h5>Total User Visit</h5>
+            <p>{data.conversionRate.userSubscribed}</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ConversionRate;
