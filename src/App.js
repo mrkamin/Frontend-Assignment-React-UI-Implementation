c9cae1fd9bf581de5b24f170743efa501d231794
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-14 08:42:28 +0500

    Commenting on the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// Import necessary components for the App
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
+// App component that renders the Navbar, Hero, and Footer components
 const App = () => (
   <div className="App">
     <Navbar />
@@ -10,4 +12,5 @@ const App = () => (
   </div>
 );
 
+// Export the App component as the default export
 export default App;
