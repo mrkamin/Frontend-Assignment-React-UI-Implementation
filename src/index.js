0e1bf1ea5ca2a73cb74973688e73c516ecaebcf1
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-14 08:42:49 +0500

    Commenting on the index component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
+// Import necessary React libraries and components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
+// Import global styles
 import './index.css';
+
+// Import the main App component and the Redux store
 import App from './App';
 import store from './redux/store';
 
+// Create a React root for rendering
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component wrapped with Redux Provider in StrictMode
 root.render(
   <React.StrictMode>
     <Provider store={store}>
