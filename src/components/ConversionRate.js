db3f119d16e30743b69decdc528ee7aff582c78b
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-15 14:09:45 +0500

    commenting the conversionRate component

--- src/components/ConversionRate.js
+++ src/components/ConversionRate.js
@@ -1,3 +1,4 @@
+// Import necessary dependencies and components
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchData } from '../redux/data/actions';
@@ -9,12 +10,18 @@ import {
 } from './SvgConversionRate';
 import '../css/conversion.css';
 
+// Define the ConversionRate component
 const ConversionRate = () => {
+  // Initialize Redux dispatch function
   const dispatch = useDispatch();
+  // Use Redux useSelector to get state variables
   const { data, loading, error } = useSelector((state) => state);
+  // useEffect hook to dispatch an action to fetch data when the component mounts
   useEffect(() => {
     dispatch(fetchData());
   }, [dispatch]);
+
+  // Conditional rendering based on loading and error states
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -26,9 +33,13 @@ const ConversionRate = () => {
       </div>
     );
   }
+
+  // Destructure data object for easier access to its properties
   const {
     userVisit, userSignUp, userSubscribed, label,
   } = data;
+
+  // Render the ConversionRate component JSX structure
   return (
     <div className="hero-container-right-side d-flex flex-column position-relative">
       <div className="right-side-uper-head d-flex">
@@ -37,6 +48,7 @@ const ConversionRate = () => {
         </div>
       </div>
       <div className="right-side-body grid2">
+        {/* Render SvgConverrsionRate component with data as props */}
         <SvgConverrsionRate
           userVisit={userVisit}
           userSignUp={userSignUp}
@@ -44,6 +56,7 @@ const ConversionRate = () => {
           label={label}
         />
         <div className="d-flex flex-column gap-4">
+          {/* Render information about Total User Visit */}
           <div className="d-flex gap-3">
             <SvgConverrsionRate1 />
             <div className="conversion-user-visit d-flex flex-column">
@@ -55,6 +68,7 @@ const ConversionRate = () => {
               </h5>
             </div>
           </div>
+          {/* Render information about User Sign Up */}
           <div className="d-flex gap-3">
             <SvgConverrsionRate2 />
             <div className="conversion-user-visit d-flex flex-column">
@@ -66,6 +80,7 @@ const ConversionRate = () => {
               </h5>
             </div>
           </div>
+          {/* Render information about User Subscribed */}
           <div className="d-flex gap-3">
             <SvgConverrsionRate3 />
             <div className="conversion-user-visit d-flex flex-column">
@@ -83,4 +98,5 @@ const ConversionRate = () => {
   );
 };
 
+// Export the ConversionRate component
 export default ConversionRate;
